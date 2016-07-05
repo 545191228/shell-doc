adf139b7cc07786e17b496602d98623da8bc1fbb
coach <shp2357@vip.qq.com>
2016-07-05 09:44:27 +0800

    修改日志创建方式

--- nginx_log.sh
+++ nginx_log.sh
@@ -10,19 +10,21 @@ YESTERDAY=`data -d yesterday +%Y%M%D`
 
 MONTHS=`data -d yesterday +%Y%M`
 
+PID_PATH=/usr/local/nginx/logs/nginx.pid
+
 # access_log
 if [ ! -d "${LOG_PATH}/${MONTHS}/access" ] then;
 	midir -p ${LOG_PATH}/${MONTHS}/access
 fi
 mv ${LOG_PATH}/access.log ${LOG_PATH}/${MONTHS}/access/access_${YESTERDAY}.log
-touch ${LOG_PATH}/access.log
 
 #error_log
 if [ ! -d "${LOG_PATH}/${MONTHS}/error" ] then;
 	midir -p ${LOG_PATH}/${MONTHS}/error
 fi
 mv ${LOG_PATH}/error.log ${LOG_PATH}/${MONTHS}/error/error_${YESTERDAY}.log
-touch ${LOG_PATH}/error.log
+
+kill -USR1 $(cat ${PID_PATH})
 
 exit
 
